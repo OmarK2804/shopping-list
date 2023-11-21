c3972aa83713e13684250fcb087c1fe3b62b6a6c
OmarK2804 <khalifomar4@gmail.com>
2023-11-21 23:36:21 +0700

    add import json

--- main/views.py
+++ main/views.py
@@ -10,6 +10,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
 
 # Create your views here.
 
